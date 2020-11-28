9d54aa79c8934632e57785fb6141f1c0949b7820
vityokklymets <vityokklymets@gmail.com>
2020-11-28 11:56:29 +0100

    ParallaxImage component

--- src/components/ParallaxImage.vue
+++ src/components/ParallaxImage.vue
@@ -1,81 +1,42 @@
 <template>
   <div
-    ref="mirrorContainerRef"
-    class="parallax__container parallax__container--mirror"
+    data-parallax="scroll"
+    :data-image-src="imageSrc"
+    :style="containerCss"
+    class="parallax__container"
   >
-    <img ref="imageRef" class="parallax__image" :src="imageSrc" />
+    <slot></slot>
   </div>
-  <div ref="containerRef" class="parallax__container"></div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+import { defineComponent, onMounted } from "vue";
+import jQuery from "jquery";
+import "jquery-parallax.js";
 
 export default defineComponent({
   name: "ParallaxImage",
   props: {
     imageSrc: String,
+    containerHeight: Number,
   },
-  setup: () => {
-    const containerRef = ref<HTMLDivElement>();
-    const mirrorContainerRef = ref<HTMLDivElement>();
-    const imageRef = ref<HTMLImageElement>();
-
-    const handleScroll = () => {
-      const { innerHeight: windowHeigth, scrollY } = window;
-
-      const speed = 0.2;
-
-      const mirrorContainer = mirrorContainerRef.value;
-      const container = containerRef.value;
-
-      if (mirrorContainer && container) {
-        const offsetTop = container.offsetTop;
-        const mirrorTranslateY = offsetTop - scrollY;
-        const traslateY = mirrorTranslateY * -speed;
-
-        if (scrollY > offsetTop - windowHeigth) {
-          mirrorContainer.style.visibility = "visible";
-          mirrorContainer.style.transform = `translate(0,${mirrorTranslateY}px)`;
-          imageRef.value!.style.transform = `translate(0,${traslateY}px)`;
-        } else {
-          mirrorContainer.style.visibility = "hidden";
-        }
-      }
-    };
-
+  setup: ({ containerHeight = 492 }) => {
     onMounted(() => {
-      window.addEventListener("scroll", handleScroll);
+      jQuery(window).trigger("resize").trigger("scroll");
     });
 
-    onUnmounted(() => {
-      window.removeEventListener("scroll", handleScroll);
-    });
-    return { containerRef, imageRef, mirrorContainerRef };
+    return {
+      containerCss: {
+        height: `${containerHeight}px`,
+      },
+    };
   },
 });
 </script>
 
 <style scoped>
 .parallax__container {
-  overflow: hidden;
-  visibility: hidden;
-  height: 492px;
-  width: 100%;
-}
-
-.parallax__container--mirror {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-}
-.parallax__image {
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 754px;
-  width: 100%;
+  background: transparent;
 }
 </style>
